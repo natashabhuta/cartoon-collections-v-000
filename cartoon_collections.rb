2c5960aa68b127faa63e8c1d51ee19efabcab395
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 21:22:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,7 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients find do |cheese|
-  
-  .include?(cheese_types)
+    cheese_types.include? 
   if true 
     return cheese_types.first
   else 
