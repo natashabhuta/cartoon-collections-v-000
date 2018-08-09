eb048273c3ddc1fe87ec4b5c52a7dfd97d4be44a
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 21:14:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? do |cheese_types| 
+  ingredients.include? do |cheese_types| 
   if true 
     return cheese.first
   else 
