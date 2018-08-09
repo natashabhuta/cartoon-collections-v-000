778354000115db31cf711c9b87472006c07afb23
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 20:50:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,7 @@ end
 end 
 
 def summon_captain_planet(planeteer_calls)
-  summons = []
-  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize} >> summons
-  summons.each {|planeteer_calls| puts summons + '!'}
+  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize} 
 end 
 
 def long_planeteer_calls# code an argument here
