d7638dbf06d5f85b1c9b81aa5c4cc630038daa82
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 20:32:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names) # code an argument here
+  roll_call = []
+ roll_call(names) do |name|
+   puts name
 end
 
 def summon_captain_planet# code an argument here
