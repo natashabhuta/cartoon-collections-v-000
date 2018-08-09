4618e2a1db6c6756b224fba2d505001d1c1eabcf
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 20:55:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 end 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect 
+  planeteer_calls.collect do |call|
+    
 end 
 
 def long_planeteer_calls# code an argument here
